f156c6390745d56f5b4f4207795832b41e161a8e
Billy Bunn <williammbunn@gmail.com>
2019-04-10 16:19:38 -0700

    adds folders

---
+++ app/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
+const faker = require('faker');
+
+setInterval( () => {
+  socket.emit('speak', faker.hacker.phrase());
+}, 750);
+
+// const fs = require('fs');
+
+// const alterFile = (file) => {
+//   fs.readFile( file, (err, data) => {
+//     if(err) { throw err; }
+//     let text = data.toString().toUpperCase();
+//     fs.writeFile( file, Buffer.from(text), (err, data) => {
+//       if(err) { throw err; }
+//       console.log(`${file} saved`);
+//     });
+//   });
+// };
+
+// let file = process.argv.slice(2).shift();
+// alterFile(file);
